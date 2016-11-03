c782c3ccb2a8d847e2caad87883a141eb1f31393
fagnerrs <fagnerrs@gmail.com>
2016-11-02 22:24:05 -0200

    Uploading project

---
+++ TimeZoneRevolution.Shared/TimeStudy.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TimeZoneRevolution.Shared
+{
+    public class TimeStudy
+    {
+        public static string getTimeZones()
+        {
+            StringBuilder builder = new StringBuilder();
+
+            var localTzi = TimeZoneInfo.Local;
+            var utcTzi = TimeZoneInfo.Utc;
+
+            //------------------------------------------
+            var dtLocalOffset = DateTimeOffset.Now;
+            var dtUTCOffset = DateTimeOffset.UtcNow;
+
+            //------------------------------------------
+
+            var dtLocalDt = DateTime.Now;
+            var dtUTCDt = DateTime.SpecifyKind(DateTime.UtcNow, DateTimeKind.Utc);
+
+            //------------------------------------------
+
+
+            var fixedUtcDt = DateTime.UtcNow;
+
+            var localDt = fixedUtcDt.AddHours(DateTimeOffset.Now.Offset.Hours);
+
+
+            builder.Append("Data UTC: ").Append(fixedUtcDt.ToString("g"))
+                    .AppendLine("\nData Local: ").Append(localDt.ToString("g"));
+
+            return builder.ToString();
+        }
+
+
+        private static void converstionTest(DateTime date)
+        {
+
+        }
+    }
+}
